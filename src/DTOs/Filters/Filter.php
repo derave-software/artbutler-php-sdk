0523a1ae9368a308fb7fcb9beb9279ac4a7d1cdf
kdoma <krzysztof@deravesoftware.com>
2022-02-02 14:30:25 +0100

    Added filter mechanism

---
+++ src/DTOs/Filters/Filter.php
@@ -0,0 +1,33 @@
+<?php
+namespace ArtbutlerPhpSdk\DTOs\Filters;
+
+class Filter
+{
+    public function __construct(
+        public string $type,
+        public string $operator,
+        public string $data,
+    ){
+    }
+
+    public static function fromArray(array $data): Filter
+    {
+        return new Filter(
+            $data['type'],
+            $data['value']['operator'],
+            $data['value']['data'],
+        );
+    }
+    
+    public function createArgumentQuery(): string
+    {
+        return '
+        {
+            type: "'.$this->type.'",
+            value: {
+                operator: '.$this->operator.',
+                data: "'.$this->data.'"
+            }
+         }';
+    }
+}
---
+++ src/DTOs/Filters/FiltersCollection.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ArtbutlerPhpSdk\DTOs\Filters;
+
+use GraphQL\RawObject;
+
+class FiltersCollection
+{
+    /**
+     * @var array<Filter>
+     */
+    public function __construct(protected array $filters = [])
+    {
+    }
+
+    public function addFilter(Filter $filter): FiltersCollection
+    {
+        $this->filters[] = $filter;
+
+        return $this;
+    }
+
+    public static function fromArray($filters): FiltersCollection
+    {
+        $filtersDTO = [];
+        foreach($filters as $filter) {
+            $filtersDTO[] = Filter::fromArray($filter);
+        }
+
+        return new static($filtersDTO);
+    }
+
+    public function createQueryArgument()
+    {
+        $filtersQuery = [];
+        foreach($this->filters as $filter) {
+            $filtersQuery[]=$filter->createArgumentQuery();
+        }
+ 
+        return new RawObject('[
+        '.implode(',', $filtersQuery).'
+                      ]');
+    }
+}
