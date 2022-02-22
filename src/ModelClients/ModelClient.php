eb073f2454890f0abc667120006d3ab5b923b795
Patryk Karczmarczyk <patryk@deravesoftware.com>
2022-02-22 16:05:38 +0100

    Moved constructor to abstract class

--- src/ModelClients/ModelClient.php
+++ src/ModelClients/ModelClient.php
@@ -6,12 +6,21 @@ use ArtbutlerPhpSdk\Repositories\WorkRepository;
 use GuzzleHttp\Promise\Promise;
 use ArtbutlerPhpSdk\GraphQLClient;
 use ArtbutlerPhpSdk\Client;
+use Illuminate\Support\Arr;
 
 abstract class ModelClient
 {
     protected GraphQLClient $apiClient;
 
-    abstract public function __construct(Client $client);
+    public function __construct(protected Client $client)
+    {
+        $this->apiClient = (new GraphQLClient($client));
+    }
 
+    public function resolvePromises(array|Promise $promises): array
+    {
+        $promises = Arr::wrap($promises);
+        return $this->client->resolvePromises($promises);
+    }
 
 }
